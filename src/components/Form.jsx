8d189432d45372a8fe5b4a1d80ea5465ab780d38
firdavsxon@gmail.com <firdavsxon@gmail.com>
2024-11-09 11:59:03 +0500

    push

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,29 +1,31 @@
 import { Button, Input } from 'antd'
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { ACTIONS } from '../redux/actions'
+import {ACTIONS} from "../redux/actions"
 
-function Form() {
-  const dispatch = useDispatch()
-  const [username, setUsername] = useState("")
-  const [age, setAge] = useState("")
+const Form = () => {
+    const dispatch = useDispatch()
+    const [username,setUsername] = useState("")
+    const [age,setAge] = useState("")
+    const likedList  = useSelector(state => state.like)
 
-  const likedList = useSelector(state => state.like)
+    function handleSubmitBtn(e){
+        e.preventDefault()
+        const data = {username,age}
+        dispatch({type:ACTIONS.create,payload:data})
+        setUsername("")
+        setAge("")
+    }
 
-  function handleSubmit(e) {
-    e.preventDefault()
-    const data = { username, age }
-    dispatch({ type: ACTIONS.create, payload: data })
-    setUsername("")
-    setAge("")
-  }
-
+    
   return (
-    <form onSubmit={handleSubmit} className='w-[500px] mx-auto mt-10 space-y-3'>
-      <Input value={username} onChange={(e) => setUsername(e.target.value)} size='large' type='text' allowClear placeholder='Enter your name' />
-      <Input value={age} onChange={(e) => setAge(e.target.value)} size='large' type='number' allowClear placeholder='Enter your age' />
-      <Button className='w-full' htmlType='submit' size='large' type='primary'>Subit</Button>
-      </form>
+    <form onSubmit={handleSubmitBtn} className='w-[500px] mx-auto mt-[110px] space-y-3'>
+        <Input required value={username} onChange={(e) => setUsername(e.target.value)} size='large' type='text' allowClear placeholder='Enter Your Name...'/>
+        <Input required value={age} onChange={(e) => setAge(e.target.value)} size='large' type='number' allowClear placeholder='Enter Your Age...'/>
+        <Button className='w-full' htmlType='submit' size='large' type='primary'>Submit</Button>
+        <Button type='default' size='large'>Liked:{likedList.length}</Button>
+    </form>
   )
 }
+
 export default Form
